db2bc2313332ea55cea59f07a5b653b0426226af
gbenga504 <daveanifowoshe@ymail.com>
2017-11-23 23:44:20 +0100

    Add Login.js to the application

---
+++ src/views/Login/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+import LayoutContainer from "../../containers/LayoutContainer";
+
+const Container = styled(LayoutContainer)`background: #292931;`;
+
+export default class Login extends React.PureComponent {
+  render() {
+    return (
+        <Container>
+            
+        </Container>
+    )
+  }
+}
