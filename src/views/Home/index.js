5edb8f9da7e182b82573561dad9c05a1e38f0d0a
gbenga504 <daveanifowoshe@ymail.com>
2017-12-12 22:47:12 +0100

    Clean codes in Home screen

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -20,23 +20,12 @@ class Home extends React.PureComponent {
     headerIcon: [{ name: "ion-power", lastIcon: true, segmentName: "logout" }]
   };
 
-  componentDidMount() {
-    console.log("i mounted", this.props);
-  }
-
-  componentDidUpdate() {
-    console.log("i updated", this.props);
-  }
-
   render() {
     return (
       <div className="d-flex flex-column" style={{ width: "100%" }}>
         <DashboardHeader iconArray={this.defaults.headerIcon} />
         <ContentPadder className="flex-column">
           <Counter items={Home.seedData.items} />
-          <button onClick={() => this.props.push({ goto: "/content" })}>
-            refresh content
-          </button>
           <div
             className="d-flex flex-column align-items-center a"
             style={{ padding: "0px 320px" }}
@@ -54,38 +43,9 @@ const HomeWithData = composer("push", {
   name: "pusher",
   options: props => ({
     variables: {
-      url: "google.com",
-      timeout: props.currentUser
+      url: "google.com"
     }
   })
-  // props: ({ home_post: { loading }, mutate }) => ({
-  //   isLoading: loading,
-  //   mutation: mutate
-  // })
 })(Home);
 
-// const HomeWithData = composer("get", {
-//   name: "home_get",
-//   options: props => ({
-//     variables: {
-//       url: "google.com",
-//       timeout: props.currentUser
-//     }
-//   }),
-//   props: ({ home_get: { refetchQuery, fetchMore, error }, currentUser }) => ({
-//     refetcher: refetchQuery,
-//     error,
-//     fetchMoreData: () => {
-//       return fetchMore({
-//         variables: { url: "babalowo.com" },
-//         updateQuery: (previousResult, { fetchMoreResult }) => {}
-//       });
-//     }
-//   })
-// })(Home);
-
-function mapStateToProps(state) {
-  return { currentUser: 5 };
-}
-
-export default connect(mapStateToProps)(HomeWithData);
+export default HomeWithData;
