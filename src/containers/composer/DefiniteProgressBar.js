87823c254ace30d650868b7f5869673f46641c10
gbenga504 <daveanifowoshe@ymail.com>
2017-12-12 18:29:24 +0100

    Add a container to the progressBar

--- src/containers/composer/DefiniteProgressBar.js
+++ src/containers/composer/DefiniteProgressBar.js
@@ -9,10 +9,22 @@ const ProgressBar = styled.div`
   width: ${props => props.progress}%;
   display: ${props => (props.progress < 100 ? "block" : "none")};
   z-index: 1000001;
-  background: #66bb6a;
+  background: #1976d2;
+`;
+const ProgressContainer = styled.div`
+  position: fixed;
+  width: 100%;
+  background: #e0e0e0;
+  display: ${props => (props.progress < 100 ? "block" : "none")};
+  height: 3px;
+  z-index: 1000001;
 `;
 
-const DefiniteProgressBar = props => <ProgressBar progress={props.progress} />;
+const DefiniteProgressBar = props => (
+  <ProgressContainer progress={props.progress}>
+    <ProgressBar progress={props.progress} />
+  </ProgressContainer>
+);
 
 DefiniteProgressBar.propTypes = {
   progress: PropTypes.number
