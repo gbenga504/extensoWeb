e17e5431a770c20ba6766431cdae9dbd2791460c
gbenga504 <daveanifowoshe@ymail.com>
2017-12-14 18:54:53 +0100

    Add List content into List component

--- src/components/List.js
+++ src/components/List.js
@@ -2,6 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import Card from "./Card";
+import { BoldText } from "./AppText";
+import Fonts from "../assets/Fonts";
+import Colors from "../assets/Colors";
 
 export default class List extends React.PureComponent {
   static propTypes = {
@@ -16,21 +19,30 @@ export default class List extends React.PureComponent {
         likes_count: PropTypes.string
       }).isRequired
     ),
-    style: PropTypes.object,
-    renderNullItem: PropTypes.element
+    style: PropTypes.object
   };
 
   render() {
-    const { dataArray, renderNullItem, style } = this.props,
+    const { dataArray, style } = this.props,
       styles = style
         ? { padding: "0px 320px", ...this.props.style }
         : { padding: "0px 320px" };
 
     return (
       <div className="d-flex flex-column align-items-center" style={styles}>
-        {dataArray.length > 0
-          ? dataArray.map((item, i) => <Card key={i} item={item} />)
-          : renderNullItem}
+        {dataArray.length > 0 ? (
+          dataArray.map((item, i) => <Card key={i} item={item} />)
+        ) : (
+          <BoldText
+            style={{
+              color: Colors.noNewsColor,
+              fontSize: Fonts.noNewsText,
+              marginTop: 30
+            }}
+          >
+            Write Your First Post !!!
+          </BoldText>
+        )}
       </div>
     );
   }
