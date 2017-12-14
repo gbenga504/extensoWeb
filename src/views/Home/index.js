3261e9c0f0d10d60504a35f2b44e8c3f6c7dfdaf
gbenga504 <daveanifowoshe@ymail.com>
2017-12-14 19:03:39 +0100

    Add content to Home component

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,64 +1,52 @@
 import React from "react";
 
+import List from "../../components/List";
 import DashboardHeader from "../../components/DashboardHeader";
 import Counter from "../../components/Counter";
-import Card from "../../components/Card";
 import ContentPadder from "../../containers/ContentPadder";
+import PageContentViewer from "../../containers/PageContentViewer";
 import { composer } from "../../containers/composer";
-import { CircularSpinner } from "../../components/Loaders";
 
 class Home extends React.PureComponent {
-  static seedData = {
-    items: [
-      { number: 15, tag: "POSTS" },
-      { number: 4, tag: "CATEGORIES" },
-      { number: 300, tag: "LIKES" }
-    ]
-  };
+  generateHeaderIcon = () => [
+    {
+      name: "ion-power",
+      lastIcon: true,
+      segmentName: "logout",
+      onClick: this.logout
+    }
+  ];
 
-  defaults = {
-    headerIcon: [{ name: "ion-power", lastIcon: true, segmentName: "logout" }]
+  logout = () => {
+    localStorage.removeItem("jwt");
+    let { history: { push } } = this.props;
+    push("/login");
   };
-
-  componentDidUpdate() {
-    console.log(this.props);
-  }
 
   renderActualData = () => {
-    let { all_news: { result }, likes_count } = this.props;
+    let { all_news: { result }, likes_count } = this.props,
+      dataArray = (result && result.message) || [];
     return (
       <ContentPadder className="flex-column">
         <Counter
           items={(likes_count.result && likes_count.result.message[0]) || {}}
         />
-        <div
-          className="d-flex flex-column align-items-center"
-          style={{ padding: "0px 320px" }}
-        >
-          {result && result.message && result.message.length !== 0 ? (
-            <Card />
-          ) : (
-            <span>No data </span>
-          )}
-        </div>
+        <List dataArray={dataArray} />
       </ContentPadder>
     );
   };
 
   render() {
-    let { all_news: { loading, isInitialDataSet } } = this.props;
+    let { all_news: { loading, isInitialDataSet, error } } = this.props,
+      errorStatus = (error && true) || false;
     return (
       <div className="d-flex flex-column" style={{ width: "100%" }}>
-        <DashboardHeader iconArray={this.defaults.headerIcon} />
-        {!isInitialDataSet && loading ? (
-          <CircularSpinner
-            size={50}
-            className="align-self-center"
-            style={{ marginTop: 200 }}
-          />
-        ) : (
-          this.renderActualData()
-        )}
+        <DashboardHeader iconArray={this.generateHeaderIcon()} />
+        <PageContentViewer
+          loading={!isInitialDataSet && loading}
+          error={!isInitialDataSet && errorStatus}
+          renderItem={this.renderActualData()}
+        />
       </div>
     );
   }
@@ -69,8 +57,7 @@ const HomeWithData = composer("get", {
   options: props => ({
     variables: {
       url: "https://agro-extenso.herokuapp.com/api/v1/admin/posts/all/9"
-    },
-    fetchPolicy: "network-only"
+    }
   })
 })(
   composer("get", {
