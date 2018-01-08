dbcb4d8809e45147dfab4b6dc5245e537018eeed
gbenga504 <daveanifowoshe@ymail.com>
2018-01-08 12:38:56 +0100

    Add comment to the utils files

--- src/utils/GeneralBasedUtils.js
+++ src/utils/GeneralBasedUtils.js
@@ -1,4 +1,11 @@
 export const GeneralBasedUtils = {
+  /**
+   * @return Object
+   * @param {Object} prevProps
+   * @param {Array} arrayOfExclusion
+   * @function santizes the passed props by composing another object of props while removing the items in the arrayOfExclusion
+   * Props to be removed from the composed new props object should be added to this array
+   */
   sanitizeProps: (prevProps, arrayOfExclusion) => {
     let newProps = Object.keys(prevProps).reduce((acc, value) => {
       if (!acc[value] && arrayOfExclusion.indexOf(value) === -1) {
@@ -9,6 +16,12 @@ export const GeneralBasedUtils = {
     return newProps;
   },
 
+  /**
+   * @return Array
+   * @param {String} postMessage
+   * @param {Object} pattern
+   * @function gets hash tags matches from a supplied string and returns an array containing the hash tags in the supplied string
+   */
   getHasTags: (postMessage, pattern) => {
     let matches = [],
       resultFromMatch = [];
@@ -20,6 +33,13 @@ export const GeneralBasedUtils = {
 
   hashTags: [],
 
+  /**
+   * @return {Object}
+   * @param {String} postMessage
+   * @function formats a posts containing hash tags and returns an object containing the
+   * formatted tags to be inserted to the DOM and a stringified JSON body to be used for tags insertion into the database
+   * This function simply formats a string containing hash tags to obtain the equivalent hash tags present in the string
+   */
   formatPostWithHashTags: postMessage => {
     let matches = GeneralBasedUtils.getHasTags(
         postMessage,
@@ -52,6 +72,11 @@ export const GeneralBasedUtils = {
     };
   },
 
+  /**
+   * @return Object
+   * @param {String} hash
+   * @function formats an hash tag by removing the # so it could be used to perfrom a search against contents in the database
+   */
   formaHashTagUrlForSearch: hash => ({
     urlParams: hash
       .replace("#", "")
@@ -59,6 +84,11 @@ export const GeneralBasedUtils = {
       .toLowerCase()
   }),
 
+  /**
+   * @return number
+   * @param {String} content
+   * @function permutates and gets the reading time for a particular post
+   */
   permutateReadingTime: content => {
     let readTime = Math.floor(parseInt(content.length) / 150);
     return readTime === 0 ? 1 : readTime;
--- src/utils/LoginUtils.js
+++ src/utils/LoginUtils.js
@@ -1,4 +1,10 @@
 export const LoginUtils = {
+  /**
+   * @return bool
+   * @param {String} username
+   * @param {String} password
+   * @function validates the login form of a particular posts against the regular expression
+   */
   loginValidation: (username, password) => {
     let pattern = /^[a-zA-Z]{1,}$/;
     return pattern.test(username) && password.length > 3;
